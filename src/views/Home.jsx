e8e04b365fda3236ab07a64e69d8418b0e626849
guy2005 <pakhawat1212@gmail.com>
2025-10-15 15:59:43 +0700

    react-5

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,13 +7,81 @@ import img4 from "../assets/imgC/img04.png";
 function Home() {
   return (
     <>
-      <div
+      <div style={{ fontFamily: "Roboto Mono" }} className="w-full h-lvh bg-[#91FFB2]">
+        <div className="w-9/12  mx-auto flex py-20 justify-between">
+
+          <div className="flex flex-col -mt-5 ">
+            <div>
+              <h1 className="text-5xl font-bold text-[#333333]">
+                My Web Desogm by
+              </h1>
+              <h1 className="text-5xl font-bold text-[#333333]">
+                React + TailwindCSS
+              </h1>
+            </div>
+
+            <div className="mt-8 mb-4 text-xl">
+              <p className="text-[#333333]">
+                Lorem ipsum dolor sit amet, <br />
+                Reprehenderit ratione ad, <br />
+                animi magni quidem excepturi <br />
+                Teneturconsectetur <br />
+              </p>
+            </div>
+            <div className="mt-5 mb-10">
+              <a
+                href="#"
+                className="bg-[#333333]  text-white rounded-4xl py-3 px-10 hover:bg-[#282828]"
+              >
+                Follow Me
+              </a>
+            </div>
+            <div className=" flex">
+              <Link to="/MypageA">
+                <img className="w-65 h-min mr-5" src={img1} alt="" />
+              </Link>
+              <Link to="/MyPageB">
+                <img className="w-65 h-auto mt-1" src={img2} alt="" />
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-between items-end -mt-50">
+            <Link to="/MyPageC">
+              <img src={img3} alt="" className="w-47.5 h-min mr-10" />
+            </Link>
+            <Link to="/MyPageHome">
+              <img src={img4} alt="" className="w-28.5 h-min mr-20" />
+            </Link>
+          </div>
+
+        </div>
+        <div className="flex flex-center justify-center -mt-15">
+         <div>
+              <h1 className=" text-[#333333]">
+                (รูปและเนื้อหาทั้งหมดใช้เพื่อการศึกษาเท่านั้น)
+              </h1>
+              <h1 className="text-[#333333] ml-13">
+              Created by NinniN SAU
+              </h1>
+              <h1 className="text-[#333333] ml-4">
+              Copyright © 2025 WoW WoW WoW
+              </h1>
+            </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default Home;
+{/*<div
         style={{ fontFamily: "Roboto Mono" }}
         className="w-full h-lvh bg-[#91FFB2]"
       >
-        {/* บน */}
+        บน
         <div className="w-8/12 border-2 border-black mx-auto flex py-20 justify-between">
-          {/* 1 */}
+          1 
           <div className="flex flex-col">
             <div>
               <h1 className="text-5xl font-bold text-[#333333]">
@@ -52,7 +120,7 @@ function Home() {
           </div>
         </div>
 
-        {/* 2 */}
+        2 
         <div className="flex ">
             <Link to="/MyPageB">
                 <img src={img3} alt="" className="w-53 h-min" />
@@ -61,17 +129,12 @@ function Home() {
                 <img src={img4} alt="" className="w-32 h-min " />
             </Link>
         </div>
-        {/* 3 */}
+         3 
 
-        {/* ล่าง */}
+         ล่าง 
         <div className="flex flex-col justify-center items-center text-lg">
           <span>รูปและเนื้อหาทั้งหมดใช้เพื่อการศึกษาเท่านั้น</span>
           <span>Created by NinniN SAU</span>
           <span>Copyright © 2025 WoW WoW WoW</span>
         </div>
-      </div>
-    </>
-  );
-}
-
-export default Home;
+      </div>*/}
--- src/views/MyPageA.jsx
+++ src/views/MyPageA.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-{/*import hello */}
-{/*import profile */}
+{/*import hello */ }
+{/*import profile */ }
 import { FaGithub } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import hello from './../assets/imgH/Logo.png'
+import img01 from './../assets/imgH/img01.png'
 function MyPageA() {
   return (
     <>
-      <div style={{fontFamily: 'Monamaniac One'}} className='w-full h-lvh flex flex-col items-center bg-black'>
+      <div style={{ fontFamily: 'Monomaniac One' }} className='w-full h-lvh flex flex-col items-center bg-black'>
         {/* ส่วน 1 เมนู */}
-        <div className='w-10/12 mx-auto flex mt-15 items-center'>
+        <div className='w-10/12 mx-auto flex mt-10 items-center'>
           <div className='flex-1 flex gap-10 text-2xl'>
             <a href="#" className='text-white'>Porfolio</a>
             <a href="#" className='text-white'>About</a>
@@ -21,24 +22,28 @@ function MyPageA() {
             <img src={hello} alt="" className="w-20" />
           </div>
           <div className='flex-1 text-white flex gap-5 justify-end text-2xl'>
-            <FaGithub/>
-            <FaInstagramSquare/>
-            <FaTwitterSquare/>
-            <FaLinkedinIn/>            
+            <FaGithub />
+            <FaInstagramSquare />
+            <FaTwitterSquare />
+            <FaLinkedinIn />
           </div>
         </div>
-        {/* ส่วน 2 ข้อความ */}        
-        <div className='flex-col text-white items-center text-center mt-23 text-4xl'>
+        {/* ส่วน 2 ข้อความ */}
+        <div className='flex-col text-white items-center text-center mt-20 text-4xl'>
           <p className='mb-5'>Somrak Sudyodjing</p>
           <p className='text-[#01D5A2] mb-5'>Freelance Web Developer</p>
           <p>Based In THAILAND</p>
         </div>
         {/* ส่วน 3  */}
-        <div>
-          <div></div>
+        <div className="w-24 h-24 rounded-full overflow-hidden border-2 border-green-400">
+          <img
+            src={img01}
+            alt="Developer"
+            className="w-full h-auto object-cover" 
+          />
         </div>
 
-                  
+
       </div>
     </>
   );
